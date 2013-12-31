3023da7855b9ef0dd2cb877c69a8dd8a44ca98c7
Nicholas <nicholas@niceguys.com>
2013-12-31 14:01:30 -0800

    Changed .html and tweetit, made sample files

--- Sample/lib/tweetit.js
+++ Sample/lib/tweetit.js
@@ -1,7 +1,7 @@
 window.tweetit = function(){
 
   var twitterHtml = {
-    button: 'You finished!<br><a style="margin: 0px auto" href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-dnt="true" data-count="none" data-text="Just finished an algorithm!! cc @HackReactor" data-via="HackReactor">Tweet</a>',
+    button: 'You finished!<br><a style="margin: 0px auto" href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-dnt="true" data-count="none" data-text="Just finished writing a clone algorithm!! cc @HackReactor" data-via="HackReactor">Tweet</a>',
     script: '<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>'
   };
 
