41812e45af17ea1e2544f5777b08b062926825a9
Peter Hayes <peter.k.hayes@gmail.com>
2014-02-23 19:22:38 -0800

    Added Monte Carlo

---
+++ MonteCarlo/src/montecarlo.js
@@ -0,0 +1,25 @@
+/*
+  YOUR TASK:  To fill in the following function.
+
+  It should return a number equal to the area of the shape.
+
+  The following methods are available to you:
+
+    createPoint: function(x, y, options)
+      -- Creates a point on the graph.
+      -- Returns an object with x and y properties.
+      -- Options include "name" and "color"
+
+    evalPoint: function(point) [whichever you like]
+      -- Returns "true" if a point is inside the shape, and false otherwise.
+
+    getWindowDimensions: function()
+      -- Returns an objects with the dimensions of the window.
+      -- Has properties x and y, each of which has properties min and max.
+*/
+
+var integrate = function() {
+
+  return 42;
+
+};
