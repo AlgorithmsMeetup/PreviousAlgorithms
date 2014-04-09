0d05aedfc991dda2b0418f774e43717dd3a7409b
bendra <bendra@topped-with-meat.com>
2014-04-08 21:21:54 -0700

    simple tree search algorithmns, using recursion

--- TreeSearch/src/DFselect.js
+++ TreeSearch/src/DFselect.js
@@ -1,1 +1,17 @@
 // Write your code here
+Tree.prototype.DFSelect = function(fn){
+	out = [];
+	depth=0;
+	dfSearch(this, depth, out, fn);
+	return out;
+};
+
+
+var dfSearch = function(tree, depth, out, fn){
+	if(fn(tree.value, depth)){
+		out.push(tree.value);
+	}
+	for(var i=0; i< tree.children.length; i++){
+		dfSearch(tree.children[i], depth + 1, out, fn);
+	};
+};
