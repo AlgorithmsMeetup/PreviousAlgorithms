ad313d19a0c8872e48165dbb05d444aa3a2c96ed
bendra <bendra@topped-with-meat.com>
2014-05-20 23:42:45 -0700

    update change.js

--- MakingChange/src/change.js
+++ MakingChange/src/change.js
@@ -15,6 +15,35 @@
 
 var coinValues = [10000, 5000, 2000, 1000, 500, 100, 50, 25, 10, 5, 1];
 
+var  cache = [[]];
+  for (_i = 0, _len = coinValues.length; _i < _len; _i++) {
+    value = coinValues[_i];
+    cache.push([]);
+  }
+
 var makeChange = function(amount) {
-  return 4;
+  return makeSomeChange(amount).length;
 };
+
+//returns array of array-of-possible-combinations
+var makeSomeChange = function(amount, coins) {
+    coins = coins || coinValues;
+    var output = [];
+    for(var i = 0; i<coins.length; i++){
+      var coinValue = coins[i];
+      if(coinValue === amount){
+        output.push([coinValue]);        
+      } else if (coinValue < amount){
+        var remainder = amount - coinValue;
+        var smallerCoins = coins.slice(i);
+        var smallerCoinChange = makeSomeChange(remainder, smallerCoins);
+        if (smallerCoinChange.length > 0){
+          for(var j = 0; j< smallerCoinChange.length; j++){
+            output.push([coinValue].concat(smallerCoinChange[j]));
+          }
+        }
+      }
+    }
+    return output;
+};
+
