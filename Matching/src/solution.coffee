1740689520975346c2ee453659aab8c8fe823fed
peterkhayes <peter.k.hayes@gmail.com>
2014-06-30 21:55:26 -0700

    solution

--- Matching/src/solution.coffee
+++ Matching/src/solution.coffee
@@ -1,11 +1,55 @@
-# # Random solution - match each human with a random puppy.  Ignores preferences.
 window.solve = (input) ->
   humanPrefs = input.humans
   puppyPrefs = input.puppies
 
-  humans = _.chain(humanPrefs).keys().shuffle().value()
-  puppies = _.chain(puppyPrefs).keys().shuffle().value()
+  humans = _.keys(humanPrefs)
+  puppies = _.keys(puppyPrefs)
 
-  return _.chain(humans).zip(puppies).map((pair) -> {human: pair[0], puppy: pair[1]}).value()
+  # Keep track of who has been matched so far.
+  matchByHuman = {}
+  matchByPuppy = {}
 
+  # To see whether we're done.
+  matchedAll = false
+  while not matchedAll
+    # Assume everyone is matched until shown otherwise.
+    matchedAll = true
 
+    # Loop over every human.
+    for human in humans
+      if not matchByHuman[human]
+        # We have an unmatched human, so set matchedAll to false
+        matchedAll = false
+
+        # get most favored puppy
+        puppy = humanPrefs[human].pop()
+
+        # puppy is not yet matched.
+        if not matchByPuppy[puppy]
+          matchByPuppy[puppy] = human
+          matchByHuman[human] = puppy
+
+        # puppy has been matched already.
+        else
+          # WHO IS MY RIVAL
+          existingMatch = matchByPuppy[puppy]
+
+          # Calculate my rank, and the rank of my rival.
+          myRank = puppyPrefs[puppy].indexOf(human)
+          existingMatchRank = puppyPrefs[puppy].indexOf(existingMatch)
+
+          # If my rank is greater, replace the pretender
+          if myRank > existingMatchRank
+            matchByHuman[existingMatch] = null
+            matchByHuman[human] = puppy
+            matchByPuppy[puppy] = human
+
+  # Make my solution
+  solution = []
+  for puppy, human of matchByPuppy
+    solution.push {human: human, puppy: puppy}
+
+  return solution
+
+# For the optimal solution, see http://www.corelab.ntua.gr/courses/netalg/pres2013/xatzidimitriou.pdf
+# But be prepared for a lot of math!
--- Matching/src/solution.js
+++ Matching/src/solution.js
@@ -1,66 +1,48 @@
-/*
-  Greetings, algorithmics!  Today, our problem is the "Best Matching Problem".
-  
-  Imagine n people go to the pound to adopt puppies, and there are (coincidentally) n puppies at the pound.
-  Each person makes a list of puppies, ranking them from least to most favorite.
-  Similarly, each puppy makes a ranking of the humans.
-
-  The challenge is to pair up puppies and humans so that the maximum amount of satisfaction is achieved.
-
-  This problem is open-ended - the solution I'll present at the end of class is just one possible answer.
-
-  ---
-
-  INPUT: (Imagine we have 3 humans and 3 puppies.)
-
-  {
-    humans: {
-      "human1": ['puppy2', 'puppy3', 'puppy1'], <-- 
-      "human2": ['puppy2', 'puppy1', 'puppy3'], <-- Rankings,
-      "human3": ['puppy1', 'puppy3', 'puppy2']  <-- 
-    },                                          <--   from
-                                                <--   WORST
-    puppies: {                                  <--    
-      "puppy1": ['human1', 'human2', 'human3'], <--    to
-      "puppy2": ['human2', 'human3', 'human1'], <--   BEST
-      "puppy3": ['human2', 'human3', 'human1']  <--
-    }
-  }
-
-  In this situation, human1 likes puppy1 the best, and puppy2 the worst.
-  
-  OUTPUT:
-    [
-      {human: "human1", puppy: "puppy1"}, <-- An array of objects,
-      {human: "human2", puppy: "puppy3"}, <-- each listing a puppy
-      {human: "human3", puppy: "puppy2"}  <-- and a human.
-    ]
-
-  ---
-  NOTES:
-  
-    - You can use Object.keys(input.humans) to get a list of all humans (and similar for puppies)
-    - You have use of the underscore.js library, which you may find useful.
-
-*/
-
-// This solution pairs humans and puppies without considering their preferences.
-// Let's see if you can do better!
 window.solve = function(input) {
   var humanPrefs = input.humans;
   var puppyPrefs = input.puppies;
-  
-  var humans = Object.keys(humanPrefs);
-  var puppies = Object.keys(puppyPrefs);
-  
-  var matches = [];
-
-  for (var i = 0; i < humans.length; i++) {
-    matches.push({
-      human: humans[i], 
-      puppy: puppies[i]
-    })
+  var humans = _.keys(humanPrefs);
+  var puppies = _.keys(puppyPrefs);
+  var matchByHuman = {};
+  var matchByPuppy = {};
+  var matchedAll = false;
+  while (!matchedAll) {
+    matchedAll = true;
+    
+    for (var i = 0, len = humans.length; i < len; i++) {
+      human = humans[i];
+      
+      if (!matchByHuman[human]) {
+        matchedAll = false;
+        var puppy = humanPrefs[human].pop();
+        
+        if (!matchByPuppy[puppy]) {
+          matchByPuppy[puppy] = human;
+          matchByHuman[human] = puppy;
+        } else {
+          var existingMatch = matchByPuppy[puppy];
+          
+          var myRank = puppyPrefs[puppy].indexOf(human);
+          var existingMatchRank = puppyPrefs[puppy].indexOf(existingMatch);
+          
+          if (myRank > existingMatchRank) {
+            matchByHuman[existingMatch] = null;
+            matchByHuman[human] = puppy;
+            matchByPuppy[puppy] = human;
+          }
+        }
+      }
+    }
   }
 
-  return matches;
+  solution = [];
+  for (puppy in matchByPuppy) {
+    human = matchByPuppy[puppy];
+    solution.push({
+      human: human,
+      puppy: puppy
+    });
+  }
+  
+  return solution;
 };
