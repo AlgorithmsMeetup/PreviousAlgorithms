a7857e0384b0747cdbcd53ea639308dd542df32a
Peter Hayes <peter.k.hayes@gmail.com>
2014-07-23 20:51:08 -0700

    finished quadtree repo

--- Quadtree/spec/spec.js
+++ Quadtree/spec/spec.js
@@ -1,24 +1,32 @@
 describe("Quadtree tests", function() {
 
-  var quadtree;
+  var quadtree, bigFilledQuadtree;
+  before(function() {
+    bigFilledQuadtree = new Quadtree(new Box(-10, -10, 610, 610));
+    for (var x = 0; x < 600; x++) {
+      for (var y = 0; y < 600; y++) {
+        bigFilledQuadtree.insert(new Point(x, y));
+      }
+    }
+  });
 
   beforeEach(function() {
     var box = new Box(-10, -10, 10, 10);
     quadtree = new Quadtree(box);
   });
 
-  describe("insertion", function() {
-    it("inserts one point without error", function() {
+  describe("#insert", function() {
+    it("inserts 1 point without error", function() {
       quadtree.insert(new Point(1, 1));
     });
 
-    it("inserts three points without error", function() {
+    it("inserts 3 points without error", function() {
       quadtree.insert(new Point(1, 1));
       quadtree.insert(new Point(0, 0));
       quadtree.insert(new Point(-2, 4));
     });
 
-    it("inserts one hundred points without error", function() {
+    it("inserts 100 points without error", function() {
       var x, y;
       for (var i = 0; i < 100; i++) {
         x = _.random(-10, 10);
@@ -27,9 +35,18 @@ describe("Quadtree tests", function() {
       }
     });
 
+    it("inserts 40000 points without error", function() {
+      var quadtree = new Quadtree(new Box(-10, -10, 310, 310));
+      for (var x = 0; x < 300; x++) {
+        for (var y = 0; y < 300; y++) {
+          quadtree.insert(new Point(x, y));
+        }
+      }
+    });
+
     it("adds a first point without creating subtrees", function() {
       quadtree.insert(new Point(0, 0));
-      expectPoint(quadtree, 0, 0);
+      expectPoint(quadtree.point, 0, 0);
       expect(quadtree.SW).to.not.be.ok();
       expect(quadtree.SE).to.not.be.ok();
       expect(quadtree.NW).to.not.be.ok();
@@ -39,12 +56,9 @@ describe("Quadtree tests", function() {
     it("adds a second point into the correct subtree", function() {
       quadtree.insert(new Point(0, 0));
       quadtree.insert(new Point(1, 1));
-      // expect(quadtree.SW).to.be.a(Quadtree);
-      // expect(quadtree.SE).to.be.a(Quadtree);
-      // expect(quadtree.NW).to.be.a(Quadtree);
       expect(quadtree.NE).to.be.a(Quadtree);
-      expectPoint(quadtree, 0, 0);
-      expectPoint(quadtree.NE, 1, 1);
+      expectPoint(quadtree.point, 0, 0);
+      expectPoint(quadtree.NE.point, 1, 1);
     });
 
     it("adds several points into the correct subtrees", function() {
@@ -63,22 +77,167 @@ describe("Quadtree tests", function() {
       expect(quadtree.NE.SW).to.be.a(Quadtree);
       expect(quadtree.NE.SE).to.be.a(Quadtree);
       expect(quadtree.NE.NE).to.be.a(Quadtree);
-      expectPoint(quadtree, 0, 0);
-      expectPoint(quadtree.SW, -3, -4);
-      expectPoint(quadtree.SE, 7, -4);
-      expectPoint(quadtree.NW, -5, 2);
-      expectPoint(quadtree.NE, 1, 1);
-      expectPoint(quadtree.NE.SW, 2, 2);
-      expectPoint(quadtree.NE.SE, 9, 2);
-      expectPoint(quadtree.NE.NE, 9, 9);
+      expectPoint(quadtree.point, 0, 0);
+      expectPoint(quadtree.SW.point, -3, -4);
+      expectPoint(quadtree.SE.point, 7, -4);
+      expectPoint(quadtree.NW.point, -5, 2);
+      expectPoint(quadtree.NE.point, 1, 1);
+      expectPoint(quadtree.NE.SW.point, 2, 2);
+      expectPoint(quadtree.NE.SE.point, 9, 2);
+      expectPoint(quadtree.NE.NE.point, 9, 9);
+    });
+  });
+
+  describe("#findPointsWithin", function() {
+    it("Finds one point in the search box.  Point: (0, 0).  Box: (-10, -10, 10, 10).", function() {
+      quadtree.insert(new Point(0, 0));
+      var points = quadtree.findPointsWithin(quadtree.box);
+      expectPoints(points, [new Point(0, 0)]);
+    });
+
+    it("Does not find point out of the search box.  Point: (0, 0).  Box: (1, 1, 3, 3)", function() {
+      quadtree.insert(new Point(0, 0));
+      var points = quadtree.findPointsWithin(new Box(1, 1, 3, 3));
+      expectPoints(points, []);
+    });
+
+    it("Finds the three points in the search box.  Points: (1, 1), (-1, 1), (0, -1).  Box: (-10, -10, 10, 10)", function() {
+      quadtree.insert(new Point(1, 1));
+      quadtree.insert(new Point(-1, 1));
+      quadtree.insert(new Point(0, -1));
+      var points = quadtree.findPointsWithin(quadtree.box);
+      expectPoints(points, [new Point(1, 1), new Point(-1, 1), new Point(0, -1)]);
+    });
+
+    it("Finds the two points in the search box.  Doesn't find the two points out of the box.  Points: (1, 1), (-1, 1), (0, -1), (-1, 2).  Box: (0, -10, 10, 10)", function() {
+      quadtree.insert(new Point(1, 1));
+      quadtree.insert(new Point(-1, 1));
+      quadtree.insert(new Point(0, -1));
+      quadtree.insert(new Point(-1, 2));
+      var points = quadtree.findPointsWithin(new Box(0, -10, 10, 10));
+      expectPoints(points, [new Point(1, 1), new Point(0, -1)]);
+    });
+
+    it("Finds 100 points, all within (-10, -10, 10, 10)", function() {
+      var x, y;
+      for (var i = 0; i < 100; i++) {
+        x = _.random(-10, 10);
+        y = _.random(-10, 10);
+        quadtree.insert(new Point(x, y));
+      }
+      var points = quadtree.findPointsWithin(quadtree.box);
+      expect(points).to.have.length(100);
+    });
+
+    it("Finds only the points inside a range.  100 points, 25 with x < -5, 75 with x >= -5, finds all points with x >= -5.", function() {
+      var x, y;
+      for (var i = 0; i < 100; i++) {
+        if (i % 4) {
+          x = _.random(-5, 10);
+          y = _.random(-10, 10);
+        } else {
+          x = _.random(-10, -6);
+          y = _.random(-10, -10);
+        }
+        quadtree.insert(new Point(x, y));
+      }
+      var points = quadtree.findPointsWithin(new Box(-5, -10, 10, 10));
+      expect(points).to.have.length(75);
+    });
+
+    it("Works quickly.  Properly searches 360000 points, finding 3000.", function() {
+      var points = bigFilledQuadtree.findPointsWithin(new Box(220, 280, 319, 309));
+      expect(points.length).to.be(3000);
+    });
+  });
+
+  describe("#findNearestPointTo", function() {
+    it("If quadtree is empty, returns null", function() {
+      var point = quadtree.findNearestPointTo(new Point(0, 0));
+      expect(point).to.be(null);
+    });
+
+    it("If quadtree contains only 1 point, (0, 0), finds that point, with a query of (0, 0).", function() {
+      quadtree.insert(new Point(0, 0));
+      var point = quadtree.findNearestPointTo(new Point(0, 0));
+      expectPoint(point, 0, 0);
+    });
+
+    it("If quadtree contains only 1 point, (0, 0), finds that point, with a query of (1, 1).", function() {
+      quadtree.insert(new Point(0, 0));
+      var point = quadtree.findNearestPointTo(new Point(1, 1));
+      expectPoint(point, 0, 0);
+    });
+
+    it("If quadtree contains 2 points, (0, 0) and (5, 5), finds (0, 0) with a query of (0, 0)", function() {
+      quadtree.insert(new Point(0, 0));
+      quadtree.insert(new Point(5, 5));
+      var point = quadtree.findNearestPointTo(new Point(0, 0));
+      expectPoint(point, 0, 0);
+    });
+
+    it("If quadtree contains 2 points, (0, 0) and (5, 5), finds (0, 0) with a query of (1, 1)", function() {
+      quadtree.insert(new Point(0, 0));
+      quadtree.insert(new Point(5, 5));
+      var point = quadtree.findNearestPointTo(new Point(1, 1));
+      expectPoint(point, 0, 0);
+    });
+
+    it("If quadtree contains 4 points, (0, 0), (5, 0), (0, 5), and (5, 5), finds (5, 0) as the closest to (3, 2)", function() {
+      quadtree.insert(new Point(0, 0));
+      quadtree.insert(new Point(5, 0));
+      quadtree.insert(new Point(0, 5));
+      quadtree.insert(new Point(5, 5));
+      var point = quadtree.findNearestPointTo(new Point(3, 2));
+      expectPoint(point, 5, 0);
+    });
+
+    it("If the quadtree has all points with x and y between 0 and 599, finds (599, 494) as the closest point to (625, 494)", function() {
+      var point = bigFilledQuadtree.findNearestPointTo(new Point(625, 494));
+      expectPoint(point, 599, 494);
+    });
+
+    it("If the quadtree has all points with x and y between 0 and 599, finds (173, 329) as the closest point to (173.4, 328.9)", function() {
+      var point = bigFilledQuadtree.findNearestPointTo(new Point(173.4, 328.9));
+      expectPoint(point, 173, 329);
+    });
+
+    it("Performs many finds quickly without error - 20 queries for random points (should be under 2000 ms)", function() {
+      for (var i = 0; i < 20; i++) {
+        var x = (_.random(1000, 5000))/10 - 0.05;
+        var y = (_.random(1000, 5000))/10 - 0.05;
+        var point = bigFilledQuadtree.findNearestPointTo(new Point(x, y));
+        expectPoint(point, Math.round(x), Math.round(y));
+      }
     });
   });
 
 });
 
-expectPoint = function(quadtree, x, y) {
-  expect(quadtree.point).to.be.a(Point);
-  expect(Object.keys(quadtree.point)).to.have.length(2);
-  expect(quadtree.point.x).to.be(x);
-  expect(quadtree.point.y).to.be(y);
+var expectPoints = function(actual, expected) {
+  expect(actual).to.have.length(expected.length);
+  actual = actual.sort(pointsArrayComparator);
+  expected = expected.sort(pointsArrayComparator);
+  for (var i = 0; i < actual.length; i++) {
+    var actualPoint = actual[i];
+    var expectedPoint = expected[i];
+    expect(actualPoint).to.have.property("x", expectedPoint.x);
+    expect(actualPoint).to.have.property("y", expectedPoint.y);
+  }
+};
+
+var pointsArrayComparator = function(a, b) {
+  var xDiff = a.x - b.x;
+  var yDiff = a.y - b.y;
+  return xDiff ? xDiff : yDiff;
+};
+
+var expectPoint = function(actual, expected) {
+  if (typeof expected === "number") {
+    expected = new Point(arguments[1], arguments[2]);
+  }
+  expect(actual).to.be.a(Point);
+  expect(Object.keys(actual)).to.have.length(2);
+  expect(actual.x).to.be(expected.x);
+  expect(actual.y).to.be(expected.y);
 };
--- Quadtree/src/quadtree.js
+++ Quadtree/src/quadtree.js
@@ -7,26 +7,107 @@ var Quadtree = function(box) {
   this.NE = null;
 };
 
+Quadtree.prototype.isEmpty = function() {
+  return !this.point;
+};
+
 Quadtree.prototype.insert = function(point) {
-  if (!this.point) {
+  if (this.isEmpty()) {
     this.point = point;
     return;
   } else {
     var quadrant = this.box.findQuadrantForPoint(point);
     if (!this[quadrant]) {
-      this[quadrant] = new Quadtree(this.box.getDimensionsForQuadrant(quadrant));
+      this[quadrant] = new Quadtree(this.box.getQuadrant(quadrant));
     }
     this[quadrant].insert(point);
   }
 };
 
-Quadtree.prototype.findWithin = function(box) {
+Quadtree.prototype.findPointsWithin = function(searchBox) {
+  var points = [];
+  if (!this.isEmpty() && searchBox.contains(this.point)) {
+    points.push(this.point);
+  }
+  if (this.SW) {
+    points = points.concat(this.SW.findPointsWithin(searchBox));
+  }
+  if (this.SE) {
+    points = points.concat(this.SE.findPointsWithin(searchBox));
+  }
+  if (this.NW) {
+    points = points.concat(this.NW.findPointsWithin(searchBox));
+  }
+  if (this.NE) {
+    points = points.concat(this.NE.findPointsWithin(searchBox));
+  }
 
+  return points;
+};
+
+Quadtree.prototype.findNearestPointTo = function(target) {
+  if (this.isEmpty()) {
+    return null;
+  }
+
+  var xDist = (this.box.maxX - this.box.minX)/10;
+  var yDist = (this.box.maxY - this.box.minY)/10;
+
+  var searchBox = new Box(target.x - xDist, target.y - yDist, target.x + xDist, target.y + yDist);
+
+  var points = this.findPointsWithin(searchBox);
+  while (points.length === 0) {
+    searchBox.expand();
+    points = this.findPointsWithin(searchBox);
+  }
+
+  var best = null;
+  var bestDist = Infinity;
+  for (var i = 0; i < points.length; i++) {
+    var point = points[i];
+    var dist = point.distanceTo(target);
+    if (dist < bestDist) {
+      bestDist = dist;
+      best = point;
+    }
+  }
+  return best;
 };
 
 var Point = function(x, y) {
   this.x = x;
   this.y = y;
+};
+
+Point.prototype.isIn = function(box) {
+  if (!box) {
+    throw new Error("No box passed to Point.isIn");
+  }
+  var inX = this.x >= box.minX && this.x <= box.maxX;
+  var inY = this.y >= box.minY && this.y <= box.maxY;
+  return inX && inY;
+};
+
+Point.prototype.distanceTo = function(point) {
+  if (!point) {
+    throw new Error("No point passed to Point.distanceTo");
+  }
+  var dx = point.x - this.x;
+  var dy = point.y - this.y;
+  return Math.sqrt(dx*dx + dy*dy);
+};
+
+Point.prototype.squareDistanceTo = function(point) {
+  if (!point) {
+    throw new Error("No point passed to Point.squareDistanceTo");
+  }
+  var dx = point.x - this.x;
+  var dy = point.y - this.y;
+  return dx*dx + dy*dy;
+};
+
+Point.prototype.toString = function() {
+  return "{x: " + this.x + ", y: " + this.y + "}";
 };
 
 var Box = function(minX, minY, maxX, maxY) {
@@ -47,18 +128,24 @@ var Box = function(minX, minY, maxX, maxY) {
 };
 
 Box.prototype.contains = function(point) {
+  if (!point) {
+    throw new Error("No point passed to Box.contains");
+  }
   var inX = point.x >= this.minX && point.x <= this.maxX;
   var inY = point.y >= this.minY && point.y <= this.maxY;
   return inX && inY;
 };
 
-Box.prototype.hasOverlap = function(that) {
+Box.prototype.overlaps = function(that) {
+  if (!that) {
+    throw new Error("No box passed to Box.overlaps");
+  }
   var xOverlap = this.maxX >= that.minX && this.minX <= that.maxX;
   var yOverlap = this.maxY >= that.minY && this.minY <= that.maxY;
   return xOverlap && yOverlap;
 };
 
-Box.prototype.getDimensionsForQuadrant = function(quadrant) {
+Box.prototype.getQuadrant = function(quadrant) {
   if (quadrant === "SW") {
     return new Box(this.minX, this.minY, this.midX, this.midY);
   } else if (quadrant === "NW") {
@@ -72,9 +159,40 @@ Box.prototype.getDimensionsForQuadrant = function(quadrant) {
   }
 };
 
+Box.prototype.shrink = function() {
+  var minX = (this.minX + this.midX)/2;
+  var minY = (this.minY + this.midY)/2;
+  var maxX = (this.maxX + this.midX)/2;
+  var maxY = (this.maxY + this.midY)/2;
+
+  this.minX = minX;
+  this.minY = minY;
+  this.maxX = maxX;
+  this.maxY = maxY;
+};
+
+Box.prototype.expand = function() {
+  var minX = 2*this.minX - this.midX;
+  var minY = 2*this.minY - this.midY;
+  var maxX = 2*this.maxX - this.midX;
+  var maxY = 2*this.maxY - this.midY;
+
+  this.minX = minX;
+  this.minY = minY;
+  this.maxX = maxX;
+  this.maxY = maxY;
+};
+
+Box.prototype.toString = function() {
+  return "{minX: " + this.minX + ", minY: " + this.minY + ", maxX: " + this.maxX + ", maxY: " + this.maxY + "}";
+};
+
 Box.prototype.findQuadrantForPoint = function(point) {
+  if (!point) {
+    throw new Error("No point passed to Box.findQuadrantForPoint");
+  }
   if (!this.contains(point)) {
-    throw new Error("Quadrant " + quadrant + " does not contain ", point);
+    throw new Error("Point" + point + " is not inside box " + this);
   }
 
   if (point.x <= this.midX && point.y <= this.midY) {
