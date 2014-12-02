b9934edef9f846bda38c7ffae316a77fe44d5c5e
Peter Hayes <peter.k.hayes@gmail.com>
2014-12-01 21:35:19 -0800

    added solution

--- TowerOfHanoi/index.js
+++ TowerOfHanoi/index.js
@@ -1,4 +1,17 @@
 var actions = require("./actions");
 var config = require("./config");
 
+var move = actions.move.bind(actions);
+
+var moveNPieces = function(n, from, to, via) {
+  if (n === 1) {
+    move(from, to);
+  } else {
+    moveNPieces(n - 1, from, via, to);
+    move(from, to);
+    moveNPieces(n - 1, via, to, from);
+  }
+};
+
+moveNPieces(config.pieces, 0, 2, 1);
 actions.finish();
