aff86684e1a533fc039bcfc2726fbb9337ed567e
Mike Luby <Michael@mLuby.com>
2015-01-12 22:18:55 -0800

    fixed file structure

--- sorting/src/mergeSort.js
+++ sorting/src/mergeSort.js
@@ -1,12 +1,10 @@
 var mergeSort = function(array){
-  // Implement mergeSort here
+  // Start by leaving mergeSort alone and only implementing merge below.
+  // Or if you're feeling bold, delete this function body and rewrite it too!
   if (array.length === 1)  { return array; }
-
-  // More than one element? Divide list into left/right parts
   var mid   = Math.floor(array.length/2);
   var left  = mergeSort(array.slice(0, mid)) || [];
   var right = mergeSort(array.slice(mid, array.length)) || [];
-
   return merge(left, right);
 };
 
